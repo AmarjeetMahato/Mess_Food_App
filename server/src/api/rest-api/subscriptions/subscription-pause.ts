970e128d72a7364966743c90f40abaf12d6588df
AmarjeetMahato <amarjitmahato016@gmail.com>
2026-04-05 17:37:49 +0530

    add the routes api in subscription-pause routes

--- server/src/api/rest-api/subscriptions/subscription-pause.ts
+++ server/src/api/rest-api/subscriptions/subscription-pause.ts
@@ -11,7 +11,9 @@ const subcriptionPauseController = container.resolve(SubscriptionPauseController
 
 router.post("/subscription-pause/create",subcriptionPauseController.createSubscriptionPause)
 router.patch("/subscription-pause/:id/update", subcriptionPauseController.updatePause)
-
-
+router.get("/subscription-pause/:id", subcriptionPauseController.getPauseById)
+router.get("/subscription-pause/get-by-userId", subcriptionPauseController.getPausesByUserId);
+router.get("/subscription-pause/get-by-subscriptionId", subcriptionPauseController.getPausesBySubscriptionId)
+router.get("/subscription-pause/:id/get-active-by-subscriptionId", subcriptionPauseController.getActivePauseBySubscriptionId)
 
 export default router
